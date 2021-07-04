08c0101bffe96aff545d09bd7406f4ce473a12f2
Corey Recai <coreycrecai@gmail.com>
2021-07-04 13:28:14 -0400

    First commit - React & Strapi setup

---
+++ underdog-analytica-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0142ed4ced76051ccec51312b730a49a'),
+    },
+  },
+});
---
+++ underdog-analytica-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '45b9928a-e277-4b9a-b2b5-d08c50c4a79e'
+};
