1c1feab72f1677bf81dc1d7f742796819c2bc8d9
muhammeta7 <muhammeta7@gmail.com>
2021-07-08 03:24:51 -0400

    update to components, fixed navbar links

--- underdog-analytica/src/App.js
+++ underdog-analytica/src/App.js
@@ -1,30 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import Nav from './UI/nav.js'
 import React, {Component} from "react";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Layout} from "./UI/Layout";
 import {Home} from "./UI/Home";
 import {About} from "./UI/About";
 import {Contact} from "./UI/Contact";
+import {NoMatch} from "./UI/NoMatch";
+import {NavigationBar} from "./UI/NavigationBar"
+import {Jumbotron} from "./UI/Jumbotron";
 
 class App extends Component {
-  render() {
-      return (
-          <React.Fragment>
-              <Layout>
-                  <Router>
-                      <Switch>
-                          <Route exact path="/" component={Home} />
-                          <Route exact path="/about" component={About} />
-                          <Route exact path="/contact" component={Contact} />
-                      </Switch>
-                  </Router>
-              </Layout>
-              {/*<Nav />*/}
-          </React.Fragment>
-      );
-  }
+    render() {
+        return (
+            <React.Fragment>
+                <Router>
+                    <NavigationBar/>
+                    <Jumbotron />
+                    <Layout>
+                        <Switch>
+                            <Route exact path="/" component={Home}/>
+                            <Route path="/about" component={About}/>
+                            <Route path="/contact" component={Contact}/>
+                            <Route path={NoMatch}/>
+                        </Switch>
+                    </Layout>
+                </Router>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
---
+++ underdog-analytica/src/UI/NoMatch.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NoMatch = () => (
+    <div>
+        <h2>404 Error!</h2>
+        <p>This page does not exist.</p>
+    </div>
+)
