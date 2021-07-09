294e6a7334764c3f525370c0c05dea52c19fb26e
muhammeta7 <muhammeta7@gmail.com>
2021-07-08 23:17:32 -0400

    home page info added

--- underdog-analytica/src/pages/Home.js
+++ underdog-analytica/src/pages/Home.js
@@ -24,7 +24,18 @@ export default class home extends Component {
                       <Col xs={12} md={7}>
                           <Card>
                               <Card.Body>
-                                  <h2>Show picks and product</h2>
+                                  <h4>Save time and bet smarter with data-driven football match predictions delivered weekly.</h4>
+                                  <p>Our Machine Learning algorithm identifies favorable underdog matchups (positive odds) across 8 major European football leagues.</p>
+                                  <br />
+                                  <div>
+                                      <p>Type email to subscribe</p>
+                                      <Button className="btn-info">Subscribe</Button>
+                                      <ul>
+                                          <li>We are currently in Beta, but plan to launch full features for the 2021-2022 season.</li>
+                                          <li>Beta subscribers will receive a 2 month free trial of our premium service after launch!</li>
+                                      </ul>
+                                  </div>
+
                               </Card.Body>
                           </Card>
                       </Col>
