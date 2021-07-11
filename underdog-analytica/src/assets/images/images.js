92be15b2b18699d5ce342c91665480406cd97a09
Corey Recai <coreycrecai@gmail.com>
2021-07-11 11:58:18 -0400

    Reset backend admin.

---
+++ underdog-analytica/src/assets/images/images.js
@@ -0,0 +1,7 @@
+import bigBoard from "./big-board.png";
+
+const images = {
+  bigBoard,
+};
+
+export default images;
--- underdog-analytica/src/pages/home/home.js
+++ underdog-analytica/src/pages/home/home.js
@@ -1,22 +1,28 @@
 import React, { Component } from "react";
+import { Helmet } from "react-helmet";
 import Navigation from "../components/nav";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import { faAngleDoubleRight } from "@fortawesome/free-solid-svg-icons";
 import "./home.css";
+import images from "../../assets/images/images";
 
 export default class Home extends Component {
   render() {
     return (
       <>
+        <Helmet>
+          <title>Underdog Analytica | Home</title>
+          <link rel="stylesheet"></link>
+        </Helmet>
         <Navigation />
         <div className="section-1">
-          <Container fluid className="section-1-container pt-5">
+          <Container fluid className="section-1-container">
             <Row>
-              <Col sm={12} md={12} lg={6}>
+              <Col className="pt-5" sm={12} md={12} lg={6}>
                 <div className="section-1-cta-text">
                   <h1 className="bold section-1-h1-text">
                     TRANSPARENT &amp; HASSLE FREE SPORTS BETTING PREDICTIONS
@@ -28,12 +34,17 @@ export default class Home extends Component {
                 </div>
                 <div className="py-3">
                   <Button className="section-1-signup-btn px-5 py-3" size="lg">
-                    Sign Up Now{"  "}
-                    <FontAwesomeIcon size="sm" icon={faChevronRight} />
+                    Sign up now &nbsp;&nbsp;
+                    <FontAwesomeIcon size="sm" icon={faAngleDoubleRight} />
                   </Button>
                 </div>
               </Col>
-              <Col sm={12} md={12} lg={6}></Col>
+              <Col className="text-center" sm={12} md={12} lg={6}>
+                <img
+                  className="section-1-big-board-img"
+                  src={images.bigBoard}
+                />
+              </Col>
             </Row>
           </Container>
         </div>
