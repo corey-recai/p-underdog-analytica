cda23d19bda590e1c74c9a4fcda932ec1d7970d1
muhammeta7 <muhammeta7@gmail.com>
2021-08-09 15:35:49 -0400

    add bet history page and relevant data

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Home from "./pages/home/home";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Layout from "./pages/betHistory/components/layout";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path={"/"} component={Home}></Route>
+        <Route exact path={"/bet-history"} component={Layout}></Route>
       </Switch>
     </BrowserRouter>
   );
