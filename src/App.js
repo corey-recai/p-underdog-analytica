6ed51bb4fe770ec5cbe91558c652e29afd176367
Corey Recai <coreycrecai@gmail.com>
2021-08-12 14:29:51 -0400

    Added amplify backend and implemented Authenticated route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import Home from "./pages/home/home";
+import Dashboard from "./pages/dashboard/dashboard";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Switch>
         <Route exact path={"/"} component={Home}></Route>
+        <Route exact path={"/dashboard"} component={Dashboard}></Route>
       </Switch>
     </BrowserRouter>
   );
