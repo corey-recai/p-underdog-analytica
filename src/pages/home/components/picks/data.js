9da71b3353ce937762bb5f6d27018f63a7eff2bb
Corey Recai <coreycrecai@gmail.com>
2021-08-20 17:51:57 -0400

    Updated homepage picks

--- src/pages/home/components/picks/data.js
+++ src/pages/home/components/picks/data.js
@@ -1,136 +1,162 @@
 export const pickColumns = [
-    {
-        dataField: 'id',
-        text: 'ID',
-        hidden: true
-    },
-    {
-        dataField: 'league',
-        text: 'League',
-    },
-    {
-        dataField: 'homeTeam',
-        text: 'Home Team',
-    },
-    {
-        dataField: 'awayTeam',
-        text: 'Away Team',
-    },
-    {
-        dataField: 'prediction',
-        text: 'Prediction',
-        // align: 'center'
-    },
-    {
-        dataField: 'odds',
-        text: 'Odds',
-        // align: 'center'
-    },
-    {
-        dataField: 'date',
-        text: 'Date',
-    }
+  {
+    dataField: "id",
+    text: "ID",
+    hidden: true,
+  },
+  {
+    dataField: "league",
+    text: "League",
+  },
+  {
+    dataField: "homeTeam",
+    text: "Home Team",
+  },
+  {
+    dataField: "awayTeam",
+    text: "Away Team",
+  },
+  {
+    dataField: "prediction",
+    text: "Prediction",
+    // align: 'center'
+  },
+  {
+    dataField: "odds",
+    text: "Odds",
+    // align: 'center'
+  },
+  {
+    dataField: "date",
+    text: "Date",
+  },
 ];
 
-
-
-
-
 export const latestPicks = [
-    {
-        id: 1,
-        league: 'Ligue 1',
-        homeTeam: 'Nantes',
-        awayTeam: 'Metz',
-        prediction: "1",
-        odds: 2.3,
-        date: '15/8/2021',
-    },
-    {
-        id: 2,
-        league: 'La Liga',
-        homeTeam: 'Celta Vigo',
-        awayTeam: 'Atletico Madrid',
-        prediction: "2",
-        odds: 2.003,
-        date: '15/8/2021',
-    },
-
-    {
-        id: 3,
-        league: 'Primeria Liga',
-        homeTeam: 'Santa Clara',
-        awayTeam: 'Moreirense',
-        prediction: "1",
-        odds: 2.57,
-        date: '15/8/2021',
-    },
-    {
-        id: 4,
-        league: 'Premier Division',
-        homeTeam: 'Derry City',
-        awayTeam: 'Dundalk',
-        prediction: "2",
-        odds: 2.289,
-        date: '15/8/2021',
-    },
-    {
-        id: 5,
-        league: 'Super Liga',
-        homeTeam: 'Odense',
-        awayTeam: 'Silkeborg',
-        prediction: "1",
-        odds: 2.003,
-        date: '15/8/2021',
-    },
-    {
-        id: 6,
-        league: 'Eliterserien',
-        homeTeam: 'Mjondalen',
-        awayTeam: 'Rosenborg',
-        prediction: "2",
-        odds: 2.03,
-        date: '15/8/2021',
-    },
-    {
-        id: 7,
-        league: 'Eliterserien',
-        homeTeam: 'Viking Stavanger',
-        awayTeam: 'Molde',
-        prediction: "2",
-        odds: 2.153,
-        date: '15/8/2021',
-    },
-    {
-        id: 8,
-        league: 'Eliterserien',
-        homeTeam: ' Lillestrom',
-        awayTeam: 'Bodo Glimt',
-        prediction: "2",
-        odds: 2.527,
-        date: '15/8/2021',
-    },
-    {
-        id: 9,
-        league: 'Premier Legue (Russia)',
-        homeTeam: '   Rubin Kazan',
-        awayTeam: 'Krylya Sovetov Samara',
-        prediction: "1",
-        odds: 2.133,
-        date: '15/8/2021',
-    },
-    {
-        id: 10,
-        league: 'Ekstraklasa',
-        homeTeam: 'Zaglebie Lubin',
-        awayTeam: 'Pogon Szczecin',
-        prediction: "2",
-        odds: 2.237,
-        date: '15/8/2021',
-    }
-]
-
-
-
-
-
+  {
+    id: 1,
+    league: "Premier League",
+    homeTeam: "FC Gomel",
+    awayTeam: "Dinamo Minsk",
+    prediction: "2",
+    odds: "2.229",
+    date: "8/21/2021",
+  },
+  {
+    id: 2,
+    league: "National League",
+    homeTeam: "Weymouth",
+    awayTeam: "Boreham Wood",
+    prediction: "2",
+    odds: "2.136",
+    date: "8/21/2021",
+  },
+  {
+    id: 3,
+    league: "Super Lig",
+    homeTeam: "Alanyaspor",
+    awayTeam: "Altay",
+    prediction: "1",
+    odds: "2.044",
+    date: "8/21/2021",
+  },
+  {
+    id: 4,
+    league: "National League",
+    homeTeam: "Solihull Moors",
+    awayTeam: "Wrexham",
+    prediction: "2",
+    odds: "2.055",
+    date: "8/21/2021",
+  },
+  {
+    id: 5,
+    league: "1st Division",
+    homeTeam: "Helsingor",
+    awayTeam: "Lyngby",
+    prediction: "2",
+    odds: "2.185",
+    date: "8/21/2021",
+  },
+  {
+    id: 6,
+    league: "Swiss Super League",
+    homeTeam: "Zurich",
+    awayTeam: "Grasshopper",
+    prediction: "1",
+    odds: "2.003",
+    date: "8/21/2021",
+  },
+  {
+    id: 7,
+    league: "Ykkonen",
+    homeTeam: "Klubi-04 Helsinki",
+    awayTeam: "Ekenas IF",
+    prediction: "2",
+    odds: "2.18",
+    date: "8/21/2021",
+  },
+  {
+    id: 8,
+    league: "Football National League",
+    homeTeam: "KAMAZ Chelny",
+    awayTeam: "Baltika Kaliningrad",
+    prediction: "2",
+    odds: "2.182",
+    date: "8/21/2021",
+  },
+  {
+    id: 9,
+    league: "Ligue 2",
+    homeTeam: "Dunkerque",
+    awayTeam: "Nimes",
+    prediction: "2",
+    odds: "2.24",
+    date: "8/21/2021",
+  },
+  {
+    id: 10,
+    league: "League One",
+    homeTeam: "Charlton",
+    awayTeam: "Wigan",
+    prediction: "1",
+    odds: "2.287",
+    date: "8/21/2021",
+  },
+  {
+    id: 11,
+    league: "League Two",
+    homeTeam: "Mansfield",
+    awayTeam: "Bradford",
+    prediction: "1",
+    odds: "2.276",
+    date: "8/21/2021",
+  },
+  {
+    id: 12,
+    league: "Championship",
+    homeTeam: "Cardiff",
+    awayTeam: "Millwall",
+    prediction: "1",
+    odds: "2.35",
+    date: "8/21/2021",
+  },
+  {
+    id: 13,
+    league: "League One",
+    homeTeam: "Rotherham",
+    awayTeam: "Sheffield Wednesday",
+    prediction: "2",
+    odds: "3.42",
+    date: "8/21/2021",
+  },
+  {
+    id: 14,
+    league: "First Division A",
+    homeTeam: "Saint Truiden",
+    awayTeam: "Kortrijk",
+    prediction: "1",
+    odds: "2.821",
+    date: "8/21/2021",
+  },
+];
