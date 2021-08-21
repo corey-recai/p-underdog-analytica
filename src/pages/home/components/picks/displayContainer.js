439b2f61e3ce3917bbb11f71f11ca655051356a3
muhammeta7 <muhammeta7@gmail.com>
2021-08-21 15:26:50 -0400

    update latest picks

--- src/pages/home/components/picks/displayContainer.js
+++ src/pages/home/components/picks/displayContainer.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import {Metrics} from "./metrics";
-import { Flex, Box } from "rebass";
+import {Box} from "rebass";
 import {Container, Row} from "react-bootstrap";
 import {Picks} from "./picks";
-import {latestPicks} from "./data";
-import {GridRow} from "../../../../components/ui-custom/gridRow";
+import {latestPicks, aug22} from "./data";
 import {Heading} from "grommet";
 
 export const DisplayContainer = () => {
-        return (
-            <Container>
-                <Row>
-                    <Box gridArea="header" background="brand" >
-                        <Heading textAlign="center" margin="small" ccolor="#2383cc">Check Out our Latest Picks</Heading>
-                    </Box>
-                </Row>
-                <Row>
-                    <Flex>
-                        <Box p={2}>
-                            <Picks data={latestPicks} />
-                        </Box>
-                    </Flex>
-                </Row>
-                <Row>
-                    <p className="home-section-2-text mx-auto">
-                        ** In the table above, you will find the predictions that our algorithm has generated for this weekend's matchups. In the prediction column, a <strong>"1"</strong> means the home team has been selected to win, <strong>"2"</strong> is the away team to win, and <strong>"x</strong>" (which doesn't apply this week) means that we expect a draw.
-                    </p>
-                </Row>
-            </Container>
-        );
+    return (
+        <Container>
+            <Row>
+                <Box gridArea="header" background="brand">
+                    <Heading textAlign="center" margin="small" ccolor="#2383cc">Check Out our Latest Picks</Heading>
+                </Box>
+            </Row>
+            <Row>
+                <Picks data={aug22} date="22/08/2021"/>
+            </Row>
+            <Row>
+                <Picks data={latestPicks} date="21/08/2021"/>
+            </Row>
+            <Row>
+                <p className="home-section-2-text mx-auto">
+                    ** In the table above, you will find the predictions that our algorithm has generated for this
+                    weekend's matchups. In the prediction column, a <strong>"1"</strong> means the home team has been
+                    selected to win, <strong>"2"</strong> is the away team to win, and <strong>"x</strong>" (which
+                    doesn't apply this week) means that we expect a draw.
+                </p>
+            </Row>
+        </Container>
+    );
 }
