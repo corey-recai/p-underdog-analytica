02fb99a4f5a1521273353c657d5379e2291479b3
muhammeta7 <muhammeta7@gmail.com>
2021-10-20 14:22:32 -0400

    update titles and everything

--- src/components/ui/customCard.js
+++ src/components/ui/customCard.js
@@ -4,28 +4,29 @@ import SubscribeBtn from "../../pages/components/subscribeBtn";
 export const CustomCard = () => {
     return (
         <Card >
-            <CardHeader className="justify-content-center home-section-1-h1-text bold mx-auto" pad="small">
-                The Future of Sports Investing
+            <CardHeader className="justify-content-center home-section-1-h1-text bold mx-auto" pad="xsmall">
+                <div className="gridcard-content-area gridcard-title">
+                     The Future of Sports: Investing
+                </div>
             </CardHeader>
-            <CardBody pad="small" className="text-left">
-                <div className="text-subsection">
+            <CardBody pad="small">
+                <div className="gridcard-subtitle gridcard-content-area">
                     <p>
                         Welcome to your honest, reliable, mathematically proven handicapper.
-                        <br />
-                        Our algorithm makes the calculations, you make the cash.
                     </p>
                 </div>
-                <div className="text-subsection">
+                <div className="gridcard-content-area">
                     <p>
+                        Our algorithm makes the calculations, you make the cash.
                         We apply a Machine Learning approach to produce European football predictions that are mathematically supported.
                         Our algorithm discovers Underdogs, or teams who are underperforming according to our model. This means that all of the predictions we provide pay out at least +2 odds.
                     </p>
-                    <p>Sign up to receive alerts for when we produce our weekend picks!</p>
+                    <p className="gridcard-content-area">Sign up to receive alerts for when we produce our weekend picks!</p>
+                </div>
+                <div>
+                    <SubscribeBtn />
                 </div>
             </CardBody>
-            <CardFooter pad={{horizontal: "small"}} background="light-5">
-                <SubscribeBtn />
-            </CardFooter>
         </Card>
     );
 }
