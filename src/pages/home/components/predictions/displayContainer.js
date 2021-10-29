ccc5d2e1bdf2533fd724542ba4becf625c2e5369
crisrod34 <33216090+crisrod34@users.noreply.github.com>
2021-10-29 12:43:25 -0400

    formatting predictions

--- src/pages/home/components/predictions/displayContainer.js
+++ src/pages/home/components/predictions/displayContainer.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import {Box, Flex} from "rebass";
-import {Container, Row, Col} from "react-bootstrap";
-import {CustomTable} from "./customTable";
 import {monday, predictionColumns, saturday, sunday} from "./data/predictionData";
 import {PredictionInfo} from "./predictionInfo";
+import Container from "react-bootstrap/Container";
+import React, {useContext} from "react";
+import {Box, Grid, ResponsiveContext, Heading} from "grommet";
+import {Row} from "react-bootstrap";
+import {CustomTable} from "./customTable";
+import {resultColumns, saturdayResults, sundayResults} from "./data/resultData";
+import {GridCard} from "../../../../components/ui/gridCard.js";
 
 
 export const DisplayContainer = () => {
+    const size = useContext(ResponsiveContext)
     return (
         <Container>
-            <Row>
-                <Flex>
-                    <Box p={2} width={[1000]} fontSize={1}>
-                        <CustomTable columns={predictionColumns} data={saturday} date="30/10/2021"/>
+          <Box pad="xsmall">
+            <Grid gap="small" columns={[size]} alignContent="center">
+              <div className="center">
+                <Row>
+                    <Box>
+                      <GridCard
+                        title="Last Week's Results & Details"
+                        content={
+                          [<CustomTable columns={predictionColumns} data={saturday} date="30/10/2021"/>,
+                          <CustomTable columns={predictionColumns} data={sunday} date="31/10/2021"/>,
+                          <CustomTable columns={predictionColumns} data={monday} date="1/11/2021"/>,
+                          <PredictionInfo />]
+                        }
+                        />
                     </Box>
-                </Flex>
-            </Row>
-            <Row>
-                <Flex>
-                    <Box p={2} width={[1000]} fontSize={1}>
-                        <CustomTable columns={predictionColumns} data={sunday} date="31/10/2021"/>
-                    </Box>
-                </Flex>
-            </Row>
-            <Row>
-                <Flex>
-                    <Box p={2} width={[1000]} fontSize={1}>
-                        <CustomTable columns={predictionColumns} data={monday} date="01/11/2021"/>
-                    </Box>
-                </Flex>
-            </Row>
-            <PredictionInfo />
+                </Row>
+              </div>
+            </Grid>
+          </Box>
         </Container>
     );
 }
