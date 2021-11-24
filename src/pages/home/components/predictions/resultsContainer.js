80af3b89fd587d9e17d530305e2d83a648b972f0
muhammeta7 <muhammeta7@gmail.com>
2021-11-24 14:16:36 -0500

    updated picks

--- src/pages/home/components/predictions/resultsContainer.js
+++ src/pages/home/components/predictions/resultsContainer.js
@@ -1,37 +1,37 @@
 import Container from "react-bootstrap/Container";
 import React, {useContext} from "react";
 import {PredictionInfo} from "./predictionInfo";
-import {Box, Grid, ResponsiveContext, Heading} from "grommet";
+import {Box, Grid, ResponsiveContext} from "grommet";
 import {Row} from "react-bootstrap";
 import {CustomTable} from "./customTable";
 import {mondayResults, resultColumns, saturdayResults, sundayResults} from "./data/resultData";
 import {GridCard} from "../../../../components/ui/gridCard.js";
 
 
-
 export const ResultsContainer = () => {
     const size = useContext(ResponsiveContext)
     return (
         <Container>
-          <Box pad="xsmall">
-            <Grid gap="small" columns={[size]} alignContent="center">
-              <div className="center">
-                <Row>
-                    <Box>
-                      <GridCard
-                        title="Last Week's Results & Details"
-                        content={
-                          [<CustomTable columns={resultColumns} data={saturdayResults} date="20/11/2021"/>,
-                          <CustomTable columns={resultColumns} data={sundayResults} date="21/11/2021"/>,
-                          <CustomTable columns={resultColumns} data={mondayResults} date="22/11/2021"/>,
-                          <PredictionInfo />]
-                        }
-                        />
-                    </Box>
-                </Row>
-              </div>
-            </Grid>
-          </Box>
+            <Box pad="xsmall">
+                <Grid gap="small" columns={[size]} alignContent="center">
+                    <div className="center">
+                        <Row>
+                            <Box>
+                                <GridCard
+                                    title="Last Week's Results & Details"
+                                    content={[
+                                        <CustomTable columns={resultColumns} data={saturdayResults} date="20/11/2021"/>,
+                                        <CustomTable columns={resultColumns} data={sundayResults} date="21/11/2021"/>,
+                                        <CustomTable columns={resultColumns} data={mondayResults} date="22/11/2021"/>,
+                                        <PredictionInfo
+                                        />
+                                    ]}
+                                />
+                            </Box>
+                        </Row>
+                    </div>
+                </Grid>
+            </Box>
         </Container>
     );
 }
