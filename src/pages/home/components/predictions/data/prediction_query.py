337d5943519ef865d6dc46a354a890e5636d4373
Cristian Rodriguez <chrodriguez34@gmail.com>
2022-01-17 13:44:37 -0500

    wrote script for predictions

---
+++ src/pages/home/components/predictions/data/prediction_query.py
@@ -0,0 +1,37 @@
+import sys
+import sqlite3
+import pandas as pd
+import json
+
+# SQL STUFF
+# get DB file
+sqlite_file = './clean.db'
+# Connecting to the database file
+conn = sqlite3.connect(sqlite_file)
+c = conn.cursor()
+c.execute("""SELECT match_date as date, league as league, home_team as home, away_team as away, prediction as prediction, sel_odds as odds FROM predictions WHERE live == 1 ORDER BY date;""")
+bets = c.fetchall()
+df = pd.DataFrame(bets, columns=["Date", "League", "HomeTeam", "AwayTeam", "Prediction", "Odds"])
+
+
+## WRITE A FUNCTION TO SEE WHAT DAY OF THE WEEK IT IS USING sys.datetime
+days = sorted(set(df.Date.values))
+weekend_predictions = {}
+for day in days:
+    weekend_predictions[day] = []
+
+for bet in bets:
+    weekend_predictions[bet[0]].append({
+        "League": bet[1],
+        "HomeTeam": bet[2],
+        "AwayTeam": bet[3],
+        "Prediction": bet[4],
+        "Odds": bet[5]
+    })
+
+j = json.dumps(weekend_predictions)
+print(j)
+
+#export JSON here
+with open ('predictions.json', 'w') as outfile:
+    outfile.write(j)
